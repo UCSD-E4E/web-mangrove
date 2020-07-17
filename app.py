6545c3bbe3945b0bb6429b70d93f243184d91f2e
nmeister <nmeister@princeton.edu>
2020-07-17 14:50:23 -0400

    trying azure stuff

--- app.py
+++ app.py
@@ -119,6 +119,10 @@ def fix_shp(filename):
     shp.to_file(filename)
     return shp
 
+def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
 
 @server.route('/', methods=['GET', 'POST'])
 def home():
