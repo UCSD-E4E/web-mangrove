f81d60e021a9ea050f945faa1649fb586354633c
nmeister <nmeister@princeton.edu>
2020-07-24 13:24:36 -0400

    hopefully fixed the delete process

--- classify_mod.py
+++ classify_mod.py
@@ -69,11 +69,14 @@ def get_batch_list(list_of_files, BATCH_SIZE):
 def delete_files_in_dir(folder):
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
+
         try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
+            '''if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+                shutil.rmtree(file_path)'''
+            print('deleting: ' + UPLOAD_FOLDER + file_path)
+            os.remove(UPLOAD_FOLDER + file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
     return
