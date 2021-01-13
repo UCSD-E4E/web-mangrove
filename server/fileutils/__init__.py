ebda6f63c7dd5bc01e941de62d34432b5a0d9029
Paul Pan <panjunhong05@gmail.com>
2021-01-13 09:01:51 +0000

    Separated out functionality of getting images.

--- server/fileutils/__init__.py
+++ server/fileutils/__init__.py
@@ -9,36 +9,39 @@ from . import constants
 
 bp = Blueprint('fileutils', __name__, url_prefix='/files')
 
-@bp.route('', methods=('GET', 'POST'))
-def get_files():
+def create_folders():
     if not os.path.exists(constants.UPLOADED_FOLDER):
         os.makedirs(constants.UPLOADED_FOLDER)
     if not os.path.exists(constants.PROCESSED_FOLDER):
         os.makedirs(constants.PROCESSED_FOLDER)
-    if request.method == 'GET':
-        files = []
-        for dirpath, _, filenames in os.walk(constants.UPLOADED_FOLDER):
-            files.extend(os.path.join(dirpath, file) for file in filenames)
-        return Response(json.dumps(files), mimetype='application/json')
-    elif request.method == 'POST':
-        files = request.data
-        content_type = request.mimetype
-        if content_type == 'application/zip':
-            with zipfile.ZipFile(BytesIO(files), 'r') as zip_ref:
-                zip_ref.extractall(constants.UPLOADED_FOLDER)
-            return 'Done Uploading!'
-        elif content_type == 'image/tiff':
-            filename = 'image-' + str(round(time() * 1000))  + '.tif'
-            query_filename = request.args.get('filename')
-            if query_filename is not None:
-                filename = query_filename
-            with open(os.path.join(constants.UPLOADED_FOLDER, filename), 'wb') as tif:
-                tif.write(files)
-            return 'Done Uploading!'
-        else:
-            return Response(json.dumps({
-                'message': 'Content-Type invalid. Only .zip and .tif files are supported.',
-                'status': 400,
-            }), status=400, mimetype='application/json')
 
-    return 'Hello World!'
+@bp.route('upload', methods=['POST'])
+def upload_files():
+    create_folders()
+    files = request.data
+    content_type = request.mimetype
+    if content_type == 'application/zip':
+        with zipfile.ZipFile(BytesIO(files), 'r') as zip_ref:
+            zip_ref.extractall(constants.UPLOADED_FOLDER)
+        return 'Done Uploading!'
+    elif content_type == 'image/tiff':
+        filename = 'image-' + str(round(time() * 1000))  + '.tif'
+        query_filename = request.args.get('filename')
+        if query_filename is not None:
+            filename = query_filename
+        with open(os.path.join(constants.UPLOADED_FOLDER, filename), 'wb') as tif:
+            tif.write(files)
+        return 'Done Uploading!'
+    else:
+        return Response(json.dumps({
+            'message': 'Content-Type invalid. Only .zip and .tif files are supported.',
+            'status': 400,
+        }), status=400, mimetype='application/json')
+
+@bp.route('list', methods=['GET'])
+def list_files():
+    create_folders()
+    files = []
+    for dirpath, _, filenames in os.walk(constants.UPLOADED_FOLDER):
+        files.extend(os.path.join(dirpath, file) for file in filenames)
+    return Response(json.dumps(files), mimetype='application/json')
