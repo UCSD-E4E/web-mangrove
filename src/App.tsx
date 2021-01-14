28056b84ffa374ecbc0c74b458e38a49217d53cb
Paul Pan <panjunhong05@gmail.com>
2021-01-13 20:47:52 -0800

    Added websocket code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { io } from 'socket.io-client';
+import axios from 'axios';
 
-function App() {
+const App: React.FC = () => {
+
+  const [socketId, setSocketId] = React.useState<string>();
+
+  React.useEffect(() => {
+    const socket = io('http://localhost:5000');
+    socket.on('connect', () => {setSocketId(socket.id)});
+    socket.on('message', (data: any) => {
+      let progress = document.getElementById('retile-progress')!;
+      progress.innerHTML = 'Progress: ' + data;
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type='file' accept='.tif,.tiff,.zip' id='fileinput' />
+      <button type='submit' onClick={() => {
+        let fileInput = document.getElementById('fileinput') as any;
+        let file = fileInput.files[0];
+        let extension = fileInput.value.split('.').pop();
+        axios.post('http://localhost:5000/files/upload', file, {
+          headers: {
+            'Content-Type': extension === 'zip' ? 'application/zip' : 'image/tiff',
+          },
+          onUploadProgress: (event) => {
+            let progress = document.getElementById('upload-progress')!;
+            progress.innerHTML = 'Progress: ' + Math.round((event.loaded * 100) / event.total) + '%';
+          }
+        });
+      }}>
+        Upload
+      </button>
+      <span id='upload-progress'>Progress: </span>
+      <button type='submit' onClick={() => {
+        console.log(socketId);
+        axios.post('http://localhost:5000/files/retile', { room: socketId })
+          .catch(error => {console.log(error.response.data)});
+      }}>
+        Retile
+      </button>
+      <span id='retile-progress'>Progress: </span>
+    </>
   );
 }
 
